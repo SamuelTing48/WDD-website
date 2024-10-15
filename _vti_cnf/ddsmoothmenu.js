12b8f59271e22163532962e1b6243ee5d896fc2b
Samuel Ting li Wei <samueltlw17@gmail.com>
2024-10-15 13:08:27 +0800

    second init

--- _vti_cnf/ddsmoothmenu.js
+++ _vti_cnf/ddsmoothmenu.js
@@ -6,4 +6,4 @@ vti_modifiedby:SR|LAPTOP-00TTMJI7\\Asus
 vti_timecreated:TR|18 Nov 2023 18:34:59 -0000
 vti_cacheddtm:TX|18 Nov 2023 18:34:59 -0000
 vti_filesize:IR|40428
-vti_backlinkinfo:VX|off-campus.html vertica.html premium++.html dual.html apartment.html premium.html fortune.html standard.html default.dwt aboutus.html default.html on-campus.html Satellite.html
+vti_backlinkinfo:VX|off-campus.html vertica.html premium++.html dual.html apartment.html premium.html fortune.html standard.html default.dwt aboutus.html index.html on-campus.html Satellite.html
