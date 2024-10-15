16ff22fdd840826ef4f896217a1f1de59c6ecc99
Samuel Ting li Wei <samueltlw17@gmail.com>
2024-10-15 12:57:30 +0800

    first init

---
+++ _vti_cnf/cometmouse.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|LAPTOP-00TTMJI7\\Asus
+vti_modifiedby:SR|LAPTOP-00TTMJI7\\Asus
+vti_timelastmodified:TR|24 Nov 2023 16:02:55 -0000
+vti_timecreated:TR|24 Nov 2023 16:02:03 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|apartment.html dual.html off-campus.html premium++.html vertica.html fortune.html premium.html standard.html default.dwt aboutus.html Satellite.html default.html on-campus.html
+vti_nexttolasttimemodified:TW|24 Nov 2023 16:02:18 -0000
+vti_cacheddtm:TX|24 Nov 2023 16:02:55 -0000
+vti_filesize:IR|1815
